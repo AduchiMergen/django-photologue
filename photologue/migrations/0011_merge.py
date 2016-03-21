dc814617bd80de4729a0b22945f6e0b4833c39eb
Artem Kanarev <artem.kanarev@invitae.com>
2016-03-21 21:36:38 +0600

    merge migration, update ru translation

---
+++ photologue/migrations/0011_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('photologue', '0010_auto_20160105_1307'),
+        ('photologue', '0009_photosize_by_max_side'),
+    ]
+
+    operations = [
+    ]
